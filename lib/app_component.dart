fb6245a7042e3e4acf738ec1d2795157b9ee3ed7
ROGER CHAN <roger.chan@cargosmart.com>
2017-05-10 16:40:25 +0800

    Before start

---
+++ lib/app_component.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2017, CHANRO3. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+
+@Component(selector: 'my-app', template: '<h1>Hello {{name}}</h1>')
+class AppComponent {
+  var name = 'Angular';
+}
